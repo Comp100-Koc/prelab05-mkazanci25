86fdf66bc1a90f2fa049a42afb36bbe688531955
mkazanci25 <mkazanci25@ku.edu.tr>
2026-04-02 21:45:38 +0300

    message

--- q1.py
+++ q1.py
@@ -2,7 +2,18 @@ def longest_palindromic_substring(s):
     """
     Given a string find the longest palindromic substring
     """
-    x =len(s)
-    max_len=0
-    a=""
-    for i in range()
+    res = ""
+
+    def expand(l, r):
+        nonlocal res
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if (r - l + 1) > len(res):
+                res = s[l:r+1]
+            l -= 1
+            r += 1
+
+    for i in range(len(s)):
+        expand(i, i)
+        expand(i, i + 1)
+
+    return res if len(res) >= 2 else ""
