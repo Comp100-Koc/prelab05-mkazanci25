9ba85fef97967b583c1c20a7bd6741c6cb654ef1
mkazanci25 <mkazanci25@ku.edu.tr>
2026-04-02 22:00:09 +0300

    message

--- q3a.py
+++ q3a.py
@@ -1,4 +1,13 @@
 def add_binary(a, b):
-    
     decimal_sum = int(a, 2) + int(b, 2)
-    return bin(decimal_sum)
+    
+    if decimal_sum == 0:
+        return "0b0"
+    
+    result = ""
+    while decimal_sum > 0:
+        kalan = decimal_sum % 2
+        result = str(kalan) + result
+        decimal_sum = decimal_sum // 2
+        
+    return "0b" + result
